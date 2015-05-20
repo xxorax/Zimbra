527f69d897fe93b176e59587fc6a77dfb02e1e01
jorgedlcruz <jorgedlcruz@gmail.com>
2015-05-20 22:27:43 +0100

    Changes

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Installing Zimbra Collaboration, time to inject the configuration" \
+/opt/zimbra/libexec/zmsetup.pl -c /tmp/zcs/installZimbraScript
 /etc/init.d/sshd start
 /etc/init.d/sshd stop
 su -c "/opt/zimbra/bin/zmcontrol start" zimbra
