d8361a4dce13843394fa6e8c9519d6f97ffd5a17
jorgedlcruz <jorgedlcruz@gmail.com>
2015-05-20 18:09:51 +0100

    Split of the Script files from the Docker file

--- docker/dns.sh
+++ docker/dns.sh
@@ -1,55 +1,29 @@
 #!/bin/sh
-#Install a DNS Server and Zimbra Collaboration 8.6
-echo "Installing DNS Server"
-sudo apt-get update && sudo sudo apt-get install -y bind9 bind9utils bind9-doc
-echo "Configuring DNS Server"
-sed "s/-u/-4 -u/g" /etc/default/bind9 > /etc/default/bind9.new
-mv /etc/default/bind9.new /etc/default/bind9
-rm /etc/bind/named.conf.options
-
-cat <<EOF >>/etc/bind/named.conf.options
-options {
-directory "/var/cache/bind";
-
-listen-on { $IP; }; # ns1 private IP address - listen on private network only
-allow-transfer { none; }; # disable zone transfers by default
-
-forwarders {
-8.8.8.8;
-8.8.4.4;
-};
-auth-nxdomain no; # conform to RFC1035
-#listen-on-v6 { any; };
-
-};
-EOF
-
-cat <<EOF >>/etc/bind/named.conf.local
-zone "$DOMAIN" {
+cat <<EOF >>named.conf.local
+zone "$1" {
         type master;
-        file "/etc/bind/db.$DOMAIN";
+        file "/etc/bind/db.$1";
 };
 EOF
 
-touch /etc/bind/db.$DOMAIN
-cat <<EOF >/etc/bind/db.$DOMAIN
+touch /etc/bind/db.$1
+cat <<EOF >/etc/bind/db.$1
 \$TTL  604800
-@      IN      SOA    ns1.$DOMAIN. root.localhost. (
+@      IN      SOA    ns1.$1. root.localhost. (
                               2        ; Serial
                         604800        ; Refresh
                           86400        ; Retry
                         2419200        ; Expire
                         604800 )      ; Negative Cache TTL
 ;
-@     IN      NS      ns1.$DOMAIN.
-@     IN      A      $IP
-@     IN      MX     10     $HOSTNAME.$1.
-$HOSTNAME     IN      A      $IP
-ns1     IN      A      $IP
-mail     IN      A      $IP
-pop3     IN      A      $IP
-imap     IN      A      $IP
-imap4     IN      A      $IP
-smtp     IN      A      $IP
+@     IN      NS      ns1.$1.
+@     IN      A      $2
+@     IN      MX     10     $3.$1.
+$3     IN      A      $2
+ns1     IN      A      $2
+mail     IN      A      $2
+pop3     IN      A      $2
+imap     IN      A      $2
+imap4     IN      A      $2
+smtp     IN      A      $2
 EOF
-sudo service bind9 restart
