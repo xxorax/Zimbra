dcbf6adc2e24e44da135f7d03037e5a282590090
jorgedlcruz <jorgedlcruz@gmail.com>
2015-05-20 22:37:36 +0100

    Trying new config

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "Installing Zimbra Collaboration, time to inject the configuration" \
+sed -i "s/XHOSTNAMEX/`hostname -f`/" /tmp/zcs/installZimbraScript
+sed -i "s/XPASSWORDX/`date | sha1sum | cut -c-8`/" /tmp/zcs/installZimbraScript
+sed -i "s/XPASSWORD2X/`date | sha1sum | cut -c-8`/" /tmp/zcs/installZimbraScript
+sed -i "s/XRANDOMX/`date | sha1sum | cut -c-8`/" /tmp/zcs/installZimbraScript
+sed -i "s/XRANDOM2X/`date | sha1sum | cut -c-8`/" /tmp/zcs/installZimbraScript
+sed -i "s/XRANDOM3X/`date | sha1sum | cut -c-8`/" /tmp/zcs/installZimbraScript
 /opt/zimbra/libexec/zmsetup.pl -c /tmp/zcs/installZimbraScript
 /etc/init.d/sshd start
 /etc/init.d/sshd stop
