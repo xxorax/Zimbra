baa2a81651f7d2f6aa3b613060bb51ea2027441b
jorgedlcruz <jorgedlcruz@gmail.com>
2015-05-21 22:15:15 +0100

    Changes

--- docker-workinprogress/start.sh
+++ docker-workinprogress/start.sh
@@ -17,15 +17,15 @@ sudo service bind9 restart
 apt-get update
 echo "Download and install Zimbra Collaboration dependencies"
 sudo apt-get install -y netcat-openbsd sudo libidn11 libpcre3 libgmp10 libexpat1 libstdc++6 libperl5.18 libaio1 resolvconf unzip pax sysstat sqlite3
-echo "Creating a tmp folder, and downloading Zimbra Collaboration 8.6"
+echo "Download Zimbra Collaboration 8.6"
 cd /tmp/zcs 
 wget http://192.168.211.1:8000/zcs-8.6.0_GA_1153.UBUNTU14_64.20141215151116.tgz
 
 ## Building and adding the Scripts keystrokes and the config.defaults
 chmod +x dns.sh
 chmod +x createInstallFile.sh
-/bin/bash dns.sh $DOMAIN $IP $HOSTNAME
-/bin/bash createInstallFile.sh $DOMAIN $IP $PASSWORD $HOSTNAME
+./dns.sh $DOMAIN $IP $HOSTNAME
+./createInstallFile.sh $DOMAIN $IP $PASSWORD $HOSTNAME
 
 ##Install the Zimbra Collaboration ##
 echo "Downloading Zimbra Collaboration 8.6"
