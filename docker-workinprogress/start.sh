eb2e897041d03929b6e9586aeb606bc298e53c0e
jorgedlcruz <jorgedlcruz@gmail.com>
2015-05-21 22:41:14 +0100

    All in the start.sh

--- docker-workinprogress/start.sh
+++ docker-workinprogress/start.sh
@@ -3,7 +3,6 @@ echo
 IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 HOSTNAME=$(hostname -a)
 DOMAIN=$(hostname -d)
-PASSWORD='Zimbra2015'
 ## Installing the DNS Server ##
 echo "Installing DNS Server"
 sudo apt-get update && sudo sudo apt-get install -y bind9 bind9utils bind9-doc
@@ -11,6 +10,50 @@ echo "Configuring DNS Server"
 sed "s/-u/-4 -u/g" /etc/default/bind9 > /etc/default/bind9.new
 mv /etc/default/bind9.new /etc/default/bind9
 rm /etc/bind/named.conf.options
+cat <<EOF >>named.conf.local
+zone "$DOMAIN" {
+        type master;
+        file "/etc/bind/db.$DOMAIN";
+};
+EOF
+
+touch db.$DOMAIN
+cat <<EOF >db.$DOMAIN
+\$TTL  604800
+@      IN      SOA    ns1.$DOMAIN. root.localhost. (
+                              2        ; Serial
+                        604800        ; Refresh
+                          86400        ; Retry
+                        2419200        ; Expire
+                        604800 )      ; Negative Cache TTL
+;
+@     IN      NS      ns1.$DOMAIN.
+@     IN      A      $IP
+@     IN      MX     10     $HOSTNAME.$DOMAIN.
+$HOSTNAME     IN      A      $IP
+ns1     IN      A      $IP
+mail     IN      A      $IP
+pop3     IN      A      $IP
+imap     IN      A      $IP
+imap4     IN      A      $IP
+smtp     IN      A      $IP
+EOF
+cat <<EOF >>named.conf.options
+options {
+directory "/var/cache/bind";
+
+listen-on { $IP; }; # ns1 private IP address - listen on private network only
+allow-transfer { none; }; # disable zone transfers by default
+
+forwarders {
+8.8.8.8;
+8.8.4.4;
+};
+auth-nxdomain no; # conform to RFC1035
+#listen-on-v6 { any; };
+
+};
+EOF
 sudo service bind9 restart 
 
 ##Install the Zimbra Collaboration OS dependencies and Zimbra package ##
@@ -22,11 +65,113 @@ cd /tmp/zcs
 wget http://192.168.211.1:8000/zcs-8.6.0_GA_1153.UBUNTU14_64.20141215151116.tgz
 
 ## Building and adding the Scripts keystrokes and the config.defaults
-chmod +x dns.sh
-chmod +x createInstallFile.sh
-./dns.sh $DOMAIN $IP $HOSTNAME
-./createInstallFile.sh $DOMAIN $IP $PASSWORD $HOSTNAME
-
+cat <<EOF >installZimbraScript
+AVDOMAIN="$DOMAIN"
+AVUSER="admin@$DOMAIN"
+CREATEADMIN="admin@$DOMAIN"
+CREATEADMINPASS="$PASSWORD"
+CREATEDOMAIN="$DOMAIN"
+DOCREATEADMIN="yes"
+DOCREATEDOMAIN="yes"
+DOTRAINSA="yes"
+EXPANDMENU="no"
+HOSTNAME="$HOSTNAME.$DOMAIN"
+HTTPPORT="8080"
+HTTPPROXY="TRUE"
+HTTPPROXYPORT="80"
+HTTPSPORT="8443"
+HTTPSPROXYPORT="443"
+IMAPPORT="7143"
+IMAPPROXYPORT="143"
+IMAPSSLPORT="7993"
+IMAPSSLPROXYPORT="993"
+INSTALL_WEBAPPS="service zimlet zimbra zimbraAdmin"
+JAVAHOME="/opt/zimbra/java"
+LDAPAMAVISPASS="$PASSWORD"
+LDAPPOSTPASS="$PASSWORD"
+LDAPROOTPASS="$PASSWORD"
+LDAPADMINPASS="$PASSWORD"
+LDAPREPPASS="$PASSWORD"
+LDAPBESSEARCHSET="set"
+LDAPHOST="$HOSTNAME.$DOMAIN"
+LDAPPORT="389"
+LDAPREPLICATIONTYPE="master"
+LDAPSERVERID="2"
+MAILBOXDMEMORY="972"
+MAILPROXY="TRUE"
+MODE="https"
+MYSQLMEMORYPERCENT="30"
+POPPORT="7110"
+POPPROXYPORT="110"
+POPSSLPORT="7995"
+POPSSLPROXYPORT="995"
+PROXYMODE="https"
+REMOVE="no"
+RUNARCHIVING="no"
+RUNAV="yes"
+RUNCBPOLICYD="no"
+RUNDKIM="yes"
+RUNSA="yes"
+RUNVMHA="no"
+SERVICEWEBAPP="yes"
+SMTPDEST="admin@$DOMAIN"
+SMTPHOST="$HOSTNAME.$DOMAIN"
+SMTPNOTIFY="yes"
+SMTPSOURCE="admin@$DOMAIN"
+SNMPNOTIFY="yes"
+SNMPTRAPHOST="$HOSTNAME.$DOMAIN"
+SPELLURL="http://$HOSTNAME.$DOMAIN:7780/aspell.php"
+STARTSERVERS="yes"
+SYSTEMMEMORY="3.8"
+TRAINSAHAM="ham.ttp7f9cwte@$DOMAIN"
+TRAINSASPAM="spam.nltbo_u3@$DOMAIN"
+UIWEBAPPS="yes"
+UPGRADE="yes"
+USESPELL="yes"
+VERSIONUPDATECHECKS="TRUE"
+VIRUSQUARANTINE="virus-quarantine.46arq7z7yi@$DOMAIN"
+ZIMBRA_REQ_SECURITY="yes"
+ldap_bes_searcher_password="$PASSWORD"
+ldap_dit_base_dn_config="cn=zimbra"
+ldap_nginx_password="$PASSWORD"
+mailboxd_directory="/opt/zimbra/mailboxd"
+mailboxd_keystore="/opt/zimbra/mailboxd/etc/keystore"
+mailboxd_keystore_password="$PASSWORD"
+mailboxd_server="jetty"
+mailboxd_truststore="/opt/zimbra/java/jre/lib/security/cacerts"
+mailboxd_truststore_password="changeit"
+postfix_mail_owner="postfix"
+postfix_setgid_group="postdrop"
+ssl_default_digest="sha256"
+zimbraFeatureBriefcasesEnabled="Enabled"
+zimbraFeatureTasksEnabled="Enabled"
+zimbraIPMode="ipv4"
+zimbraMailProxy="FALSE"
+zimbraMtaMyNetworks="127.0.0.0/8 $IP/24 [::1]/128 [fe80::]/64"
+zimbraPrefTimeZoneId="America/Los_Angeles"
+zimbraReverseProxyLookupTarget="TRUE"
+zimbraVersionCheckNotificationEmail="admin@$DOMAIN"
+zimbraVersionCheckNotificationEmailFrom="admin@$DOMAIN"
+zimbraVersionCheckSendNotifications="TRUE"
+zimbraWebProxy="FALSE"
+zimbra_ldap_userdn="uid=zimbra,cn=admins,cn=zimbra"
+zimbra_require_interprocess_security="1"
+INSTALL_PACKAGES="zimbra-core zimbra-ldap zimbra-logger zimbra-mta zimbra-snmp zimbra-store zimbra-apache zimbra-spell zimbra-memcached zimbra-proxy"
+EOF 
+cat <<EOF >installZimbra-keystrokes
+y
+y
+y
+y
+n
+y
+y
+y
+y
+y
+y
+y
+EOF
 ##Install the Zimbra Collaboration ##
 echo "Downloading Zimbra Collaboration 8.6"
 cd /tmp/zcs && tar xzvf zcs-*
